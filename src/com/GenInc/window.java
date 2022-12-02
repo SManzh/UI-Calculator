6e08f483c514ec73f34fdab125fa6790789ae395
Vyacheslav Manzhela <slavamanzh@gmail.com>
2022-12-02 12:12:37 +0300

    init: start ui calculator project with draft

---
+++ src/com/GenInc/window.java
@@ -0,0 +1,57 @@
+package com.GenInc;
+
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.EventListener;
+
+public class window extends JFrame implements WindowListener, EventListener {
+
+    public window() {
+        setLayout(null);
+        setVisible(true);
+        setSize(400,600);
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+}
